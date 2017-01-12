fe213cd8b03e516fae1bc07691822f2db3da4950
Dror Atariah <drorata@gmail.com>
2017-01-12 07:22:44 +0100

    Added the fork of the alchemy theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,6 +19,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = 'themes/pelican-alchemy/alchemy'
+
 # Blogroll
 LINKS = (
     ('Pelican', 'http://getpelican.com/'),
