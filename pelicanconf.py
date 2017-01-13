ef0a816ce9f089044dafa673370ba9e22759ec29
Dror Atariah <drorata@gmail.com>
2017-01-13 13:53:21 +0100

    Modification to config

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Dr. Dror Atariah'
+AUTHOR = 'Dror Atariah'
 SITENAME = 'Dr. Dror'
-SITEURL = ''
+SITESUBTITLE = 'Foo is not just bar'
+SITEURL = 'https://drorata.github.io'
 
 PATH = 'content'
 
@@ -21,7 +22,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ('Pelican', 'http://getpelican.com/'),
+    ('LinkedIn', 'https://www.linkedin.com/in/atariah'),
     #('You can modify those links in your config file', '#'),
     )
 
@@ -30,19 +31,26 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
-
+DEFAULT_CATEGORY = 'General'
 DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
 
 STATIC_PATHS = [
     'images',
+    'files',
     'extra/favicon.ico'
 ]
 EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'},
 }
+
+PLUGIN_PATHS = ["plugins"]
+#PLUGINS = ["render_math"]
 
 THEME = 'themes/pelican-alchemy/alchemy'
 SITEIMAGE = '/images/colored-spiral-of-roots.png width=90 height=90'
